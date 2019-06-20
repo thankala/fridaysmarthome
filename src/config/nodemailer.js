1efd9fc4950258c6aa3664273a3098014fbdebf3
Thanasis Kalatheris <43992756+thankala@users.noreply.github.com>
2019-06-20 20:49:00 +0300

    Update nodemailer.js

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'fridaysmarthome@gmail.com',
-        pass: 'fridaypassword123456'
+        user: ,
+        pass:,
     }
 });
 
